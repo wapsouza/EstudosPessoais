b65a7a01b6bfba45701b18503fabffe18a94e814
wapsouza <threedaysdying@hotmail.com>
2017-11-06 02:52:57 -0200

    update

--- core/views.py
+++ core/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import request
 from django.http import HttpResponse
+from .forms import *
 
 def index(request):
     return render(request,'index.html')
+
+def contato (request):
+    if request.POST:
+        form = ContatoForm(request.POST)
+        if form.is_valid():
+	            form.envia_email()
+    else:
+        form = ContatoForm()
+    context = {
+		"contato.html":form
+    }
+    return render(request,"contato.html", context)
+
+
